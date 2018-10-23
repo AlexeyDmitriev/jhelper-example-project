5746d818c0b26a0629d0dec536cea67eb0a533fb
emli <emilbeksulaymanov@gmail.com>
2018-10-23 23:42:39 +0600

    Solved one problem from UVA

--- testrunner/main.cpp
+++ testrunner/main.cpp
@@ -1,4 +1,4 @@
-#include "../tasks/cf/189/A.cpp"
+#include "../tasks/uva/10139Factovisors.cpp"
 
 #include <iostream>
 #include <fstream>
@@ -29,7 +29,7 @@ bool check(std::string expected, std::string actual) {
 
 int main() {
 	std::vector<jhelper::Test> tests = {
-		{"5 5 3 2\n", "2\n", true, true},{"7 5 5 2\n", "2\n", true, true},
+		{"6 9\n6 27\n20 10000\n20 100000\n1000 1009\n", "9 divides 6!\n27 does not divide 6!\n10000 divides 20!\n100000 does not divide 20!\n1009 does not divide 1000!\n", true, true},
 	};
 	bool allOK = true;
 	int testID = 0;
@@ -48,7 +48,7 @@ int main() {
 			std::stringstream in(test.input);
 			std::ostringstream out;
 			std::clock_t start = std::clock();
-			A solver;
+			Factovisors solver;
 			solver.solve(in, out);
 			std::clock_t finish = std::clock();
 			double currentTime = double(finish - start) / CLOCKS_PER_SEC;
