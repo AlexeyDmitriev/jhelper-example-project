bf9fbc2fbce8934bc3fc32847497c3e5e1d23ff0
emli <emilbeksulaymanov@gmail.com>
2018-11-28 09:38:27 +0600

    fixed untracked files

---
+++ cmake-build-debug/CMakeFiles/output.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/output.dir/tasks/uva/divisibility.cpp.o"
+  "output.pdb"
+  "output"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
